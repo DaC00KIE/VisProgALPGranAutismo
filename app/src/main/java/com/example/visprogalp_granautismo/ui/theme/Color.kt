1ae856433bd999b507ad88d30b3dd48badec4ef1
NessaW16 <146686207+NessaW16@users.noreply.github.com>
2023-12-10 21:08:50 +0700

    theme

--- app/src/main/java/com/example/visprogalp_granautismo/ui/theme/Color.kt
+++ app/src/main/java/com/example/visprogalp_granautismo/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.visprogalp_granautismo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightPinkBG = Color(0xFFF7D2D6)
+val SuperLightPinkBG = Color(0xFFFDE5E8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPinkBG = Color(0xFFD3A7AC)
+val DarkLightPinkBG = Color(0xFFEED1D4)
+
+val LightGrayFont = Color(0xFF9C9C9C)
+
+val LightPurple = Color(0x8065598E)
+val Purple = Color(0xFF968BBD)
+
